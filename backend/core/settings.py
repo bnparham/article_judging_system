c4cb00f989fad6578341862a98a7532730463b23
bnparham <parhambn.workmail@gmail.com>
2024-11-15 18:00:58 +0330

    feat(account): add account module to installed apps
    The account module has been created and registered in the settings.py under the installed apps section.
    
    Branch : feature/account
    Issue: #2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # === installed apps ===
+    'account',
     # === third party modules ===
     'rest_framework',
+
 ]
 
 MIDDLEWARE = [
