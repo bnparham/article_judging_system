2349755bbfa5b2ff9de27bc9f7f2f31004ad9df8
bnparham <parhambn.workmail@gmail.com>
2024-11-23 17:41:45 +0330

    fix(admin): hide 'سایر اطلاعات' fieldset in add view
    This commit ensures that the 'سایر اطلاعات' fieldset, which includes custom fields like 'failed_login_attempts' and 'get_last_failed_login_jalali', is only shown in the user edit view (change view) and not in the add user view.
    also change custom action names to persian
    add two method do admin panel : get_readonly_fields, get_fieldsets
    This commit adds a condition to the `get_date_joined_jalali`, `get_last_login_jalali`, `get_last_failed_login_jalali`, `get_last_password_reset_jalali` method to check if the `date_joined` field is available for the user object. If the date is not available, it returns the string "ثبت نشده است" instead of attempting to format a `None` value.
    Resolves #8

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -39,8 +39,8 @@ class UserAdmin(admin.ModelAdmin):
 
     # Display columns in the list view
     list_display = ['username', 'email', 'first_name', 'last_name',
-                    'get_last_login_jalali', 'last_login_ip',
-                    'failed_login_attempts', 'is_active', 'is_staff', 'verify_account', 'get_date_joined_jalali']
+                    'get_last_login_jalali',
+                    'is_active', 'is_staff', 'verify_account', 'get_date_joined_jalali']
 
     # Filters available in the list view
     list_filter = ['is_active', 'is_staff', 'verify_account', 'failed_login_attempts', 'date_joined', MonthFilter]
@@ -50,16 +50,11 @@ class UserAdmin(admin.ModelAdmin):
 
     # Read-only fields in the form view
     readonly_fields = ['get_last_login_jalali',
+                       'failed_login_attempts',
                        'last_login_ip',
                        'password_reset_attempts',
-                       'failed_login_attempts',
                        'get_last_failed_login_jalali',
                        'get_last_password_reset_jalali']
-
-    # Fields to display in the form view
-    # fields = ['username', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_staff',
-    #           'verify_account', 'phone_number', 'address', 'profile_image', 'last_login', 'last_failed_login',
-    #           'failed_login_attempts', 'last_login_ip', 'date_joined']
 
     # Fieldsets to group fields logically in the form view
     fieldsets = (
@@ -89,19 +84,45 @@ class UserAdmin(admin.ModelAdmin):
     # Actions for batch processing
     actions = ['reset_failed_login_attempts', 'lock_user_account']
 
+    def get_readonly_fields(self, request, obj=None):
+        # If `obj` is None, it's the "Add" view; otherwise, it's the "Change" view
+        if obj is None:
+            # Return an empty list of readonly fields in the Add view
+            return []
+        return self.readonly_fields
+
+    def get_fieldsets(self, request, obj=None):
+        # If `obj` is None, it's the Add view
+        if obj is None:
+            # Exclude the "سایر اطلاعات" fieldset
+            return (
+                (None, {
+                    'fields': ('username', 'email', 'first_name', 'last_name',)
+                }),
+                (_('دسترسی ها'), {
+                    'fields': ('is_active', 'is_staff', 'verify_account')
+                }),
+                (_('اطلاعات تماس'), {
+                    'fields': ('phone_number', 'address')
+                }),
+            )
+        # Show all fieldsets (default) in the Change view
+        return super().get_fieldsets(request, obj)
+
     # Custom action to reset failed login attempts
     def reset_failed_login_attempts(self, request, queryset):
         updated_count = queryset.update(failed_login_attempts=0)
         self.message_user(request, f'{updated_count} user(s) had their failed login attempts reset.')
 
-    reset_failed_login_attempts.short_description = _('Reset Failed Login Attempts')
+    reset_failed_login_attempts.short_description = _('ریست کردن تعداد لاگین های ناموفق برای کاربران انتختاب شده')
 
     # Custom action to lock the user account (e.g., if too many failed attempts)
+
     def lock_user_account(self, request, queryset):
         updated_count = queryset.update(is_active=False)
         self.message_user(request, f'{updated_count} user(s) have been locked due to too many failed login attempts.')
 
-    lock_user_account.short_description = _('Lock User Account')
+    lock_user_account.short_description = _('قفل حساب کاربری برای کاربران انتخاب شده')
 
     # Additional custom methods to display in list view
     # def get_queryset(self, request):
@@ -111,17 +132,29 @@ class UserAdmin(admin.ModelAdmin):
 
     @admin.display(description='تاریخ عضویت', ordering='date_joined')
     def get_date_joined_jalali(self, obj):
-        return datetime2jalali(obj.date_joined).strftime('%a, %d %b %Y | %H:%M:%S')
+        if obj.date_joined :
+            return datetime2jalali(obj.date_joined).strftime('%a, %d %b %Y | %H:%M:%S')
+        else:
+            return "ثبت نشده است"
 
     @admin.display(description='آخرین ورود به سیستم', ordering='last_login')
     def get_last_login_jalali(self, obj):
-        return datetime2jalali(obj.last_login).strftime('%a, %d %b %Y | %H:%M:%S')
+        if obj.last_login:
+            return datetime2jalali(obj.last_login).strftime('%a, %d %b %Y | %H:%M:%S')
+        else:
+            return "ثبت نشده است"
 
     @admin.display(description='آخرین تلاش ناموفق', ordering='last_failed_login')
     def get_last_failed_login_jalali(self, obj):
-        return datetime2jalali(obj.last_failed_login).strftime('%a, %d %b %Y | %H:%M:%S')
+        if obj.last_failed_login:
+            return datetime2jalali(obj.last_failed_login).strftime('%a, %d %b %Y | %H:%M:%S')
+        else:
+            return "ثبت نشده است"
 
     @admin.display(description='آخرین بازنشانی رمز عبور', ordering='last_password_reset')
     def get_last_password_reset_jalali(self, obj):
-        return datetime2jalali(obj.last_password_reset).strftime('%a, %d %b %Y | %H:%M:%S')
+        if obj.last_password_reset:
+            return datetime2jalali(obj.last_password_reset).strftime('%a, %d %b %Y | %H:%M:%S')
+        else:
+            return "ثبت نشده است"
 
