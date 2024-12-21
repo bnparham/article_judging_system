dfbe1a89ceae5c74fd31a8a0235b2a673e570da7
bnparham <parhambn.workmail@gmail.com>
2024-12-21 13:08:20 +0330

    issue #17 update supervisors foreignkey dropdown views & urls to fix adding new objects

--- backend/assignment/urls.py
+++ backend/assignment/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin/session/filter_supervisors/<int:schedule_id>/<int:sessionId>/', views.filter_supervisors, name='filter_supervisors'),
+    path('admin/session/filter_supervisors/<int:schedule_id>/<str:sessionId>/', views.filter_supervisors,
+         name='filter_supervisors'),
 ]
--- backend/assignment/views.py
+++ backend/assignment/views.py
@@ -7,26 +7,37 @@ from itertools import chain
 
 def filter_supervisors(request, schedule_id, sessionId):
     schedule = get_object_or_404(Schedule, id=schedule_id)
+    unique_supervisors = []
+    if sessionId == 'add':
+        # Query supervisors not assigned to the same schedule in other sessions
+        supervisors_not_in_other_sessions = Teacher.objects.filter(
+            ~Q(supervisor1_assignments__schedule=schedule) &
+            ~Q(supervisor2_assignments__schedule=schedule) &
+            ~Q(supervisor3_assignments__schedule=schedule) &
+            ~Q(supervisor4_assignments__schedule=schedule)
+        )
+        combined_supervisors = list(chain(supervisors_not_in_other_sessions))
+        unique_supervisors = {supervisor.id: supervisor for supervisor in combined_supervisors}.values()
+    else:
+        # Query supervisors assigned to the current session's schedule
+        supervisors_in_current_session = Teacher.objects.filter(
+            Q(supervisor1_assignments__schedule=schedule, supervisor1_assignments__id=sessionId) |
+            Q(supervisor2_assignments__schedule=schedule, supervisor2_assignments__id=sessionId) |
+            Q(supervisor3_assignments__schedule=schedule, supervisor3_assignments__id=sessionId) |
+            Q(supervisor4_assignments__schedule=schedule, supervisor4_assignments__id=sessionId)
+        )
 
-    # Query supervisors assigned to the current session's schedule
-    supervisors_in_current_session = Teacher.objects.filter(
-        Q(supervisor1_assignments__schedule=schedule, supervisor1_assignments__id=sessionId) |
-        Q(supervisor2_assignments__schedule=schedule, supervisor2_assignments__id=sessionId) |
-        Q(supervisor3_assignments__schedule=schedule, supervisor3_assignments__id=sessionId) |
-        Q(supervisor4_assignments__schedule=schedule, supervisor4_assignments__id=sessionId)
-    )
+        # Query supervisors not assigned to the same schedule in other sessions
+        supervisors_not_in_other_sessions = Teacher.objects.filter(
+            ~Q(supervisor1_assignments__schedule=schedule) &
+            ~Q(supervisor2_assignments__schedule=schedule) &
+            ~Q(supervisor3_assignments__schedule=schedule) &
+            ~Q(supervisor4_assignments__schedule=schedule)
+        )
 
-    # Query supervisors not assigned to the same schedule in other sessions
-    supervisors_not_in_other_sessions = Teacher.objects.filter(
-        ~Q(supervisor1_assignments__schedule=schedule) &
-        ~Q(supervisor2_assignments__schedule=schedule) &
-        ~Q(supervisor3_assignments__schedule=schedule) &
-        ~Q(supervisor4_assignments__schedule=schedule)
-    )
-
-    # Combine results and remove duplicates
-    combined_supervisors = list(chain(supervisors_in_current_session, supervisors_not_in_other_sessions))
-    unique_supervisors = {supervisor.id: supervisor for supervisor in combined_supervisors}.values()
+        # Combine results and remove duplicates
+        combined_supervisors = list(chain(supervisors_in_current_session, supervisors_not_in_other_sessions))
+        unique_supervisors = {supervisor.id: supervisor for supervisor in combined_supervisors}.values()
 
     # Prepare response with supervisor data
     supervisor_data = [{"id": supervisor.id, "name": supervisor.user.name} for supervisor in unique_supervisors]
